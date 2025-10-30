af387b53687a625c9c5f45488c69aa88f0f152a0
nisi <nishimura.ryota.s5@dc.tohoku.ac.jp>
2025-10-30 21:18:46 +0900

    Delete slice_stack directory

--- slice_stack/tsmesh_h/get_tsmesh_h.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-shot=191486
-diag="tsmesh_h"
-
-url="https://exp.lhd.nifs.ac.jp/opendata/LHD/webapi.fcgi?cmd=getfile&diag=${diag}&shotno=${shot}&subno=1"
-echo "${diag}_${shot}.txt"
-wget -q -O "${diag}_${shot}.txt" "$url"
